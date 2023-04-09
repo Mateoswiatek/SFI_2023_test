718e6fbc3148a7f847ae8a298ebf9a0b0a72a07a
Mati <mswiatek2002@gmail.com>
2023-04-09 16:30:59 +0200

    programik

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
-from tkinter import Tk
 import time
 import random
 import string
 
 login = "lessie32@10minut.xyz"  # lessie32@10minut.xyz  schuyler62@10minut.xyz
-haslo = "?^2H,3$]-bo]a)_vhT2xYj,*%wqf+A"
+password = "?^2H,3$]-bo]a)_vhT2xYj,*%wqf+A"
 
 def find_xpath(xpath):  # definiujemy se funcjkę , gdzie podajemy xpatha w "" i nam znajduje
     try:
         global browse
         try:  # po tym czasie rezygnuje
-            element = WebDriverWait(browse, 10).until(
-                EC.presence_of_element_located((By.XPATH, xpath))
+            WebDriverWait(browse, 10).until(
+                ec.presence_of_element_located((By.XPATH, xpath))
             )
         finally:  # zwraca nam element przeglądarkowy, dopisujemy kropkę i tyle
             return browse.find_element(By.XPATH, xpath)  # takie coś: find_xpath("xpath_elementu") .click()
     except:
         print('wywalilo się')
 
-global browse
 browse = webdriver.Firefox()  # przeglądarka, wiadomo
 
 def my_setup():
@@ -30,9 +28,7 @@ def my_setup():
     # handle1 = browse.current_window_handle  # handle pierwszej strony ()
     time.sleep(random.randint(2, 5))
     find_xpath('//*[@id="username"]').send_keys(login) #email
-    find_xpath('//*[@id="password"]').send_keys(haslo)
-    time.sleep(random.randint(2, 5))
-    find_xpath('//*[@id="rememberMe"]').click()
+    find_xpath('//*[@id="password"]').send_keys(password)
     time.sleep(random.randint(2, 5))
     find_xpath('//*[@id="kc-login"]').click()
 
